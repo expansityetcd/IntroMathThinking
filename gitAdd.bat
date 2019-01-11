f5f1e30f0ed3cc3874be2b5b0db080311a332a8b
Expansity.etcd <Expansity.etcd@gmail.com>
2019-01-11 08:05:33 +0200

    : 8:05:32.93

--- gitAdd.bat
+++ gitAdd.bat
@@ -2,5 +2,5 @@
 for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
 set mytime=%time%
 git add .
-git commit -m " + %mydate%:%mytime% + "
+git commit -m "%mydate%:%mytime%"
 git push origin master
